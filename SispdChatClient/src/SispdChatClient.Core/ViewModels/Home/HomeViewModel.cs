1d930a291d0c5a7afea4530b372501d096cda9dc
sp.wang <wsp33251@gmail.com>
2020-08-29 09:47:00 +0900

    init

---
+++ SispdChatClient/src/SispdChatClient.Core/ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SispdChatClient.Core.ViewModels.Home
+{
+    public class HomeViewModel : BaseViewModel
+    {
+    }
+}
